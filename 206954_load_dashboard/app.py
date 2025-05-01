bd612869253de60d3b83aacbad17e40bdd3ae7a6
Matthew Cristaldi <162157149+Cristaldi4@users.noreply.github.com>
2025-05-01 12:41:54 -0400

    Update app.py

--- 206954_load_dashboard/app.py
+++ 206954_load_dashboard/app.py
@@ -1,5 +1,5 @@
 # app.py
 import 206954_load_report as report  # Use the correct file name (update accordingly)
 
-# Run the Streamlit app logic from load_report_206954.py
+# Run the Streamlit app logic from _206954_load_report.py
 report.run_dashboard()
